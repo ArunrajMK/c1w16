022c9cbe448b33f95a0911049623f67907a6a153
ArunrajMK <arunrajshanker6@gmail.com>
2022-04-11 14:03:16 +0530

    Done

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,9 +1,14 @@
+import React from "react";
 
+// const [count] = 0
 // keep the add to cart component here
 const CartButton = () => {
+  // const {title} = props
     //manage state of the count 
   return <>
+  <button>Add to Cart</button>
   {/* add to cart button */}
+  
   {/* count with - and  + button */}
   <div>
       <button>-</button>
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,17 +1,43 @@
 import React from "react"
 import data from '../data.json'
+import "./Grocerydetails.css"
+
 
 
 const GroceryDetails = ()=>{
-console.log(data)
+console.log("data",data)
 
     return(
         <>
         <h1>Groceries</h1>
         <div className="container">
+            {data.map((item)=>{
+                return <div  className="main"> 
+                    <img src ={item.imgURL} alt="title" />
+                    <h4>{item.title}</h4>
+                    <div className="bottom">
+                        <h4>{item.mrp}</h4>
+                        <h4>{item.sellingPrice}</h4>
+                    </div>
+
+                </div>
+            })}
+            {/* <CartButton data = {data}/> */}
+
             {/* map through the data and display the cards */}
         </div>
         </>
     )
 }
 export default GroceryDetails
+
+
+// {
+//     "id": 1,
+//     "title": "Nivea Whitening Smooth Skin Roll On Deodorant for Women 50 ml",
+//     "imgURL":
+//       "https://www.jiomart.com/images/product/150x150/490830561/nivea-whitening-smooth-skin-roll-on-deodorant-for-women-50-ml-product-images-o490830561-p490830561-0-202203151142.jpg",
+//     "mrp": "₹ 199.00",
+//     "sellingPrice": "₹ 132.00",
+//     "discount": "33%"
+//   },
